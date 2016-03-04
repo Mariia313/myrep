2ba80e675d391832ce196e97f574b8d17b8381dc
Mariia313 <marusyna313@ukr.net>
2016-03-04 17:06:37 +0200

    new Main

---
+++ aaaa/src/main/java/Mav/aaaa/Main.java
@@ -0,0 +1,12 @@
+package Mav.aaaa;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String s = "Hello";
+		System.out.println(s);
+		System.out.println("by-by main ;(");
+
+	}
+
+}
