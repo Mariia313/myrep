1a4dda157e6398229ccfd3bc7912eb0686d68147
Mariia313 <marusyna313@ukr.net>
2016-03-04 16:54:49 +0200

    fsdjfkh

---
+++ aaaa/src/main/java/Mav/aaaa/App.java
@@ -0,0 +1,13 @@
+package Mav.aaaa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
