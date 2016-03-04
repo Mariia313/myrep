79dcbc8639c3d5ce4a9aca80406f6266a3aa64a5
Mariia313 <marusyna313@ukr.net>
2016-03-04 17:19:53 +0200

    12sd

--- aaaa/src/main/java/Mav/aaaa/Main.java
+++ aaaa/src/main/java/Mav/aaaa/Main.java
@@ -3,8 +3,8 @@ package Mav.aaaa;
 public class Main {
 
 	public static void main(String[] args) {
-		String s = "Hello";
-		System.out.println(s);
+		int i = 15;
+		System.out.println(i);
 		System.out.println("by-by main ;(");
 
 	}
